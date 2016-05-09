bb8616766833cf3645a74563e45ed49e55013271
Sonny Lazuardi <sonnylazuardi@gmail.com>
2016-05-09 17:09:12 +0700

    add css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+var styles = require('./style/main.css');
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <h1>Halo Dunia!</h1>
+            <div className="card">
+                <h1>Halo!</h1>
                 <p>Selamat belajar ReactJS</p>
             </div>
         );
--- webpack.config.js
+++ webpack.config.js
@@ -7,17 +7,20 @@ module.exports = {
         './src/index.js'
     ],
     module: {
-        loaders: [{
-            test: /\.js?$/,
-            exclude: /node_modules/,
-            loader: 'babel',
-            query: {
-                presets: ['react', 'es2015']
-            }
-        }]
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            { test: /\.css$/, loader: "style!css" }
+        ]
     },
     resolve: {
-        extensions: ['', '.js', '.jsx']
+        extensions: ['', '.js', '.jsx', '.css']
     },
     output: {
         path: __dirname + '/dist/',
